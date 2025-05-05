9339d7dd4a792c7a30716ac15f6a924f35ab10b2
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-05-05 22:23:24 +0600

    custom keybord shortcut set with ctrl+shift+enter and also output and input file separated for vew and input

--- week-1 (Basic C programming concepts)/day-one/hello.c
+++ week-1 (Basic C programming concepts)/day-one/hello.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello world");
+    char firststr[] = "Recently I heard that you’ve achieved 95% marks in your exam.";
+    char secondstr[] = "This is brilliant!";
+    char thirdstr[] = "I wish you’ll shine in your life!";
+    printf("%s\n %s %s", firststr, secondstr, thirdstr);
     return 0;
 }
+
+ 
+// This is brilliant! 
+// I wish you’ll shine in your life!	Good luck with all the barriers(/\) in your life
