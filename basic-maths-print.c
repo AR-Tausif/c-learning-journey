7e0af1190ea23be34ac69ec96acfc920fafb03c6
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2025-06-13 10:57:00 +0000

    scan number input and printing is even or odd

--- basic-maths-print.c
+++ basic-maths-print.c
@@ -3,14 +3,10 @@
 int main()
 {
     int a, b;
-    scanf("%d", &a);
-    scanf("%d", &b);
-
-
-    printf("%d + %d = %d\n", a, b, a+b);
-    printf("%d - %d = %d\n", a, b, a-b);
-    printf("%d * %d = %d\n", a, b, a*b);
-    printf("%d / %d = %.2f\n", a, b, (float)a/b); // cast integer to floatig point number
-
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    printf("%d - %d = %d\n", a, b, a - b);
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %.2f\n", a, b, (float)a / b); // cast integer to floatig point number
     return 0;
 }
