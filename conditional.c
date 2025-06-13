eacf85c1d586443c124e042553f892e93756ba3e
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2025-06-13 03:42:47 +0000

    arithmatic operator practicing and also conditional if else ladder practices with funny line printed

---
+++ conditional.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if(tk >= 100)
+    {
+        printf("Chill & chili te singara khabo 7pcs");
+    }
+    else if(tk >=48)
+    {
+        printf("local hotel e singara khabo :D");
+    }
+    else
+    {
+        printf("Areee dhurrr. kichui khabo na. Basay giye ghum dibo");
+    }
+    return 0;
+}
