9a5f3200978a78cdf44577708c5a9b6e8e648028
Tausif Ahmed <120933222+AR-Tausif@users.noreply.github.com>
2025-06-20 17:01:49 +0000

    solved: p-first-digit problem set from the online platform of codeforce

---
+++ week-2 (Basic C problem solving)/day-two-problem-solving-codeforce/p-first-digit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+  int x;
+  scanf("%d", &x);
+  int firstDigit = x;
+  while(firstDigit >= 10)
+  {
+    firstDigit = firstDigit / 10;
+  }
+
+  if(firstDigit % 2 == 0)
+  {
+    printf("EVEN");
+  }
+  else
+  {
+    printf("ODD");
+  }
+  return 0;
+}
