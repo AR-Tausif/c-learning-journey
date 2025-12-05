7a015082c9809db79a12eefeb507ac21fb55ff69
Tausif Ahmed <tausif.ritu1@gmail.com>
2025-12-05 11:06:45 +0600

    range based array tried

---
+++ string.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int* p;
+ 
+int main()
+{
+    string s = "Hello";
+    cout << s;
+
+    for(char c:s)
+    {
+        cout <<c;
+    }
+    return 0;
+}
