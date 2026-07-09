b9978c3ca5fc800c6db23d4a3b8feab065025b1e
Tausif Ahmed <tausif.ritu1@gmail.com>
2026-07-09 10:03:35 +0600

    deleted at head a node in doubly linklist with checking base cases

---
+++ delete-from-head.cpp
@@ -0,0 +1,102 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+
+    
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << ": Forwarded" << endl;   
+}
+
+void print_backward(Node* tail)
+{
+    Node* temp = tail;
+    while(temp!= NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << ": Backwarded";
+}
+
+void insert_new_node(Node* &head, Node* &tail, int val)
+{
+    Node* new_node = new Node(val);
+    if(tail==NULL)
+    {
+        tail = new_node;
+    }
+    new_node->next = head;
+    head->prev = new_node;
+    head = new_node;
+    
+}
+
+
+void insert_on_tail(Node* &head, Node* &tail, int val)
+{
+    Node* new_node = new Node(val);
+    if(head == NULL)
+    {
+        head = new_node;
+    }
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void delete_at_head(Node* &head, Node* &tail )
+{
+    
+    Node* deleted_node = head;
+    head = head->next;
+    delete deleted_node;
+    if(head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+    
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* tail = new Node(50);
+    // connecting the nodes
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = tail;
+    tail->prev = b;
+    // delete at position
+    
+    print_forward(head);
+    print_backward(tail);
+    return 0; 
+}
