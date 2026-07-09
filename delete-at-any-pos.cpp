3316cd7473e2ae54e9a8761f6c6781e10d4761a5
Tausif Ahmed <tausif.ritu1@gmail.com>
2026-07-09 11:06:37 +0600

    delete any position in doubly linked list

---
+++ delete-at-any-pos.cpp
@@ -0,0 +1,116 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+
+    
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+void print_forward(Node* head)
+{
+    Node* temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << ": Forwarded" << endl;   
+}
+
+void print_backward(Node* tail)
+{
+    Node* temp = tail;
+    while(temp!= NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << ": Backwarded";
+}
+
+void insert_new_node(Node* &head, Node* &tail, int val)
+{
+    Node* new_node = new Node(val);
+    if(tail==NULL)
+    {
+        tail = new_node;
+    }
+    new_node->next = head;
+    head->prev = new_node;
+    head = new_node;
+    
+}
+
+
+void insert_on_tail(Node* &head, Node* &tail, int val)
+{
+    Node* new_node = new Node(val);
+    if(head == NULL)
+    {
+        head = new_node;
+    }
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void delete_at_tail(Node* &head, Node* &tail)
+{
+  Node* deletedNode = tail;
+  tail = tail->prev;
+  delete deletedNode;
+  if(tail == NULL)
+  {
+    head = NULL;
+    return;
+  }
+  tail->next = NULL;
+}
+void delete_at_any_pos(Node* head, int idx)
+{
+    Node* temp = head;
+    for(int i = 0; i<idx; i++)
+    {
+        temp = temp->next;
+    }
+    cout << temp->val << endl;
+    temp->prev->next = temp->next;
+    temp->next->prev = temp->prev;
+    delete temp;
+}
+
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* tail = new Node(50);
+    // connecting the nodes
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = tail;
+    tail->prev = b;
+    // delete at position
+    delete_at_any_pos(head, 2);
+    delete_at_any_pos(head, 1);
+
+    
+    
+    print_forward(head);
+    print_backward(tail);
+    return 0; 
+}
